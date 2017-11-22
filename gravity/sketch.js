a1ea85e2ab1c70750c85b73555fd639133a6bcfd
wassim-saadallah <wassim.saadalla@gmail.com>
2017-11-22 22:47:06 +0100

    moify shit

--- gravity/sketch.js
+++ gravity/sketch.js
@@ -1,23 +1,32 @@
+// what's on the screen
 let particle;
+let target;
 let gravityFields = [];
+//number of gravity fields
 let num = 4;
-let play = false;
-let dragged = false;
-let initVel;
+//radius of the gravity field
 let rad = 75;
 
-let target;
+//start oving the ball trigger
+let play = false;
+let dragged = false;
+
+let max = rad + 100;
+
+
+
 
 function setup() {
-	createCanvas(1000, 600);
+	createCanvas(innerWidth, innerHeight);
+	//init the ball, target and the gravity fields
 	for (var i = 0; i < num; i++) {
 		gravityFields.push(createVector(random(-3 * width / 8, 3 * width / 8), random(-height / 2, height / 4)));
 	}
-
-	target = createVector(random(-width/2+20, width/2 - 20),random(-height/2 + 20, height/2 - 20))
+	target = createVector(random(-width/2+20, width/2 - 20),random(-height/2 + 20, height/2 - 20));
 	particle = new Particle(0, (height / 4) + 50);
 }
 
+//pulling the ball
 function mouseDragged() {
 	dragged = true;
 }
@@ -26,12 +35,14 @@ function mousePressed() {
 	dragged = true;
 }
 
+//releasing the ball
 function mouseReleased() {
+	let initVel;
 	dragged = false;
 	play = true;
 	initVel = createVector(mouseX-width/2, mouseY-height/2).sub(particle.pos).mult(-1);
 	console.log(initVel);
-	particle.vel = initVel.setMag(map(dist(mouseX, mouseY, particle.pos.x, particle.pos.y), 0, height, 0, 10));
+	particle.vel = initVel.setMag(map(dist(mouseX, mouseY, particle.pos.x, particle.pos.y), 0, height, 0, 5));
 	
 }
 
@@ -39,6 +50,8 @@ function mouseReleased() {
 function draw() {
 	background(220);
 	translate(width / 2, height / 2);
+
+	//drawing the gravity fields
 	noStroke();
 	for (let i = 0; i < gravityFields.length; i++) {
 		let r = gravityFields[i];
@@ -54,16 +67,20 @@ function draw() {
 		ellipse(r.x, r.y, rad + 40, rad + 40);
 	}
 
+	//Drawing the target
 	fill(255,50,10);
 	noStroke();
 	ellipse(target.x,target.y, 20,20);
 
-	if(rad>20) rad -= 1
-	else if(rad == 20) rad = 75
+	//moving the outer circles of the gravity fields
+	if(rad>20) rad -= 1;
+	else if(rad == 20) rad = 75;
 
+	//rawing the initial position of the ball
 	if (!play)
 		particle.show();
 
+	//dragging the ball
 	if (dragged && !play) {
 		strokeWeight(1);
 		stroke(0);
@@ -71,11 +88,12 @@ function draw() {
 		line(particle.pos.x, particle.pos.y, a.x, a.y);
 	}
 
+	//ball released
 	if (play) {
-		let max = Math.max(...gravityFields.map(x => dist(x.x, x.y, particle.pos.x, particle.pos.y)));
+		//max = Math.max(...gravityFields.map(x => dist(x.x, x.y, particle.pos.x, particle.pos.y)));
 		for (let i = 0; i < gravityFields.length; i++) {
 			let r = gravityFields[i];
-			particle.applyForce(r.copy().sub(particle.pos).setMag(map(dist(r.x, r.y, particle.pos.x, particle.pos.y), max, 0, 0, 0.09)));
+			particle.applyForce(r.copy().sub(particle.pos).setMag(map(dist(r.x, r.y, particle.pos.x, particle.pos.y), max, 0, 0, 0.01)));
 		}
 		particle.update();
 		particle.show();
--- project2/sketch.js
+++ project2/sketch.js
@@ -1,97 +1,22 @@
-let bubbles = [];
-let numBubbles = 100;
-let numClusters = numBubbles;
-let clusters = [];
 
-let m = 0;
-
-
-
+var offset = 50;
 function setup() {
-	frameRate(30);
-	createCanvas(800,800);
-
-	//instanciate bubbles
-	for(let i = 0; i<numBubbles; i++){
-		bubbles.push(new Bubble(random(width)-10 ,random(height)-10,7));
-	}
-
-	//instanciate the clusters
-	for(let j = 0; j<numClusters; j++){
-		let c = new Cluster([bubbles[j]],color(random(255),random(255),random(255),10));
-		c.updateColor();
-		clusters.push(c);
-	}
-
-	show();
+	createCanvas(700, 700);
 }
 
 function draw() {
-	background(0);
-
-	//check if number of clusters >1
-
-	if(clusters.length>1){
-
-		
-		//find the least distance pair
-		let min = Infinity;
-		let indexA = 0;
-		let indexB = 0;
-		for(let i = 0; i<clusters.length;i++){
-			for(let j = 0; j< clusters.length; j++){
-				if(i!=j){
-					let d = distance(clusters[i], clusters[j]);
-					if(d< min){
-						min = d; 
-						indexA = i;
-						indexB = j;
-					}  
-				}
-			}
-		}
-		
-		console.log(min, indexA, indexB);
-
-		//merge them together
-		merge(indexA,indexB);
-
-	}
-
-	show();
-}
-
-
-function distance(c1,c2){
-	var min = Infinity;
-	for(let i = 0; i<c1.bubbles.length;i++){
-		for(let j = 0; j< c2.bubbles.length; j++){
-			let d = dist(c1.bubbles[i].pos.x, c1.bubbles[i].pos.y, c2.bubbles[j].pos.x, c2.bubbles[j].pos.y);
-			if(d< min){
-				min = d; 
-			}  
+	background(240);
+	noFill();
+	translate(width/2,height/2);
+	for (var i = -6; i < 4; i+=1) {
+		for (var j = -6; j < 4; j+=1) {
+			beginShape(TRIANGLE_STRIP);
+			vertex(offset * (1 + j), offset * (1 + i),0);
+			vertex(offset * (1 + j), offset * (2 + i),0);
+			vertex(offset * (2 + j), offset * (1 + i),0);
+			endShape();
 		}
 	}
-	return min;
-}
-
-function merge(i,j){
-	let x = clusters[i].bubbles.concat(clusters[j].bubbles);
-	clusters[i].bubbles = x;
-	var c1 = clusters[i].color.levels;
-	var c2 = clusters[j].color.levels;
-	clusters[i].color = color(floor((c1[0] + c2[0])/2),floor((c1[1] + c2[1])/2),floor((c1[2] + c2[2])/2),c1[3]+10);
-	clusters[i].updateColor();
-	clusters.splice(j,1);
-}
 
 
-
-
-function show(){
-	//show the clusters and the nodes
-	clusters.forEach((c,i) => c.bubbles.forEach(b => {
-		b.show();
-	}));
-	
 }
