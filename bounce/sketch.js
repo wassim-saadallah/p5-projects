1f5b8e7eb21540b1f57963fcf63dafdf7ca282cf
wassim-saadallah <wassim.saadalla@gmail.com>
2018-11-11 20:28:18 +0100

    fireworks

---
+++ bounce/sketch.js
@@ -0,0 +1,28 @@
+var fireworks = [];
+var gravity;
+
+function setup() {
+  createCanvas(600, 500);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2);
+  stroke(255);
+  strokeWeight(4);
+}
+
+function draw() {
+  colorMode(RGB);
+  background(0, 0, 0, 25);
+  
+  if (random(1) < 0.05) {
+    fireworks.push(new Firework());
+  }
+  
+  for (var i = fireworks.length - 1; i >= 0; i--) {
+    fireworks[i].update();
+    fireworks[i].show();
+    
+    if (fireworks[i].done()) {
+      fireworks.splice(i, 1);
+    }
+  }
+}
